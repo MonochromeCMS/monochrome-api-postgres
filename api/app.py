b5a26a1dd3c866f519ec4dccba79775cc6a5b228
Carlos Ruiz <d34dplayer@protonmail.com>
2021-11-08 00:08:49 +0100

    Bump version to 1.4.1

--- api/app.py
+++ api/app.py
@@ -17,7 +17,7 @@ global_settings = get_settings()
 
 log = logging.getLogger(__name__)
 
-app = FastAPI(title="Monochrome", version="1.4.0")
+app = FastAPI(title="Monochrome", version="1.4.1")
 
 
 def get_remote_address(request: Request):
